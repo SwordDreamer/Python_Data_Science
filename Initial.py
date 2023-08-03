f57b9a1b871c92058c9bee6a10cb1ba6d69b66a8
Leonardo Joanes <leonardojoanes@gmail.com>
2023-08-03 01:22:08 -0300

    Aula Inicial - Conhecendo o Jupyter

--- Initial.py
+++ Initial.py
@@ -1,1 +1,19 @@
-print("Qual seu nome?")
+import pandas as pd
+
+# idade = [30, 27, 31, 45, 50, 22, 18, 26, 30]
+# soma = sum(idade)
+
+# print(soma)
+
+# idade.sort(reverse=True)
+# print(idade)
+
+# idade.sort()
+# print(idade)
+
+# contagem = idade.count(30)
+# print(contagem)
+
+
+base = pd.read_excel(r"/home/sworddreamer/Downloads/base_amostra_prova_pbi")
+print(base)
