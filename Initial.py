6a04157fc62edceda0b13ecbdbe8379570526278
Leonardo Joanes <leonardojoanes@gmail.com>
2023-08-03 01:34:13 -0300

    Deletando a importação

--- Initial.py
+++ Initial.py
@@ -1,19 +1,13 @@
-import pandas as pd
+idade = [30, 27, 31, 45, 50, 22, 18, 26, 30]
+soma = sum(idade)
 
-# idade = [30, 27, 31, 45, 50, 22, 18, 26, 30]
-# soma = sum(idade)
+print(soma)
 
-# print(soma)
+idade.sort(reverse=True)
+print(idade)
 
-# idade.sort(reverse=True)
-# print(idade)
+idade.sort()
+print(idade)
 
-# idade.sort()
-# print(idade)
-
-# contagem = idade.count(30)
-# print(contagem)
-
-
-base = pd.read_excel(r"/home/sworddreamer/Downloads/base_amostra_prova_pbi")
-print(base)
+contagem = idade.count(30)
+print(contagem)
